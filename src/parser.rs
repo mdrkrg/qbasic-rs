802aea6ad2d92cd7d43d7ea43c106e5ba445121e
MidoriKurage <kuragectl@gmail.com>
2026-03-03 11:10:16 +0800

    feat(parser): Implement rest of the parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 /// The parser for QBasic. It should be input a sequence of Token and output a sequence of Line.
 use crate::{
-    ast::{Expr, Line, LiteralValue, Stmt},
-    token::{Keyword, Token},
+    ast::{BinaryOp, Expr, Line, LiteralValue, Stmt, UnaryOp},
+    token::{Keyword, Math, Relational, Side, Token},
 };
 use anyhow::{Result, bail};
 use std::collections::VecDeque;
@@ -128,7 +128,22 @@ impl Parser {
     /// In QBasic, a conditional is only a conditional operator
     /// with expressions on both sides
     fn conditional(&mut self) -> Result<Expr> {
-        todo!()
+        let left = self.expression()?;
+
+        let op = match self.advance() {
+            Some(Token::Equal) => Relational::Eq,
+            Some(Token::Relational(relation)) => relation,
+            Some(token) => bail!("Expected relational operators, got {token}"),
+            None => bail!("Unexpected EOF"),
+        };
+
+        let right = self.expression()?;
+
+        Ok(Expr::Binary {
+            operator: BinaryOp::Relational(op),
+            left: Box::new(left),
+            right: Box::new(right),
+        })
     }
 
     /// expression -> term
@@ -138,26 +153,111 @@ impl Parser {
 
     /// term -> factor ( ( "-" | "+" ) factor )*
     fn term(&mut self) -> Result<Expr> {
-        todo!()
+        let mut expr = self.factor()?;
+
+        loop {
+            let operator = match self.peek() {
+                Some(Token::Operator(op))
+                    if matches!(op, Math::Plus | Math::Minus | Math::Modulo) =>
+                {
+                    *op
+                }
+                _ => break,
+            };
+            let _ = self.advance();
+
+            let right = self.factor()?;
+            let prev_expr = expr;
+            expr = Expr::Binary {
+                operator: BinaryOp::Arithmetic(operator),
+                left: Box::new(prev_expr),
+                right: Box::new(right),
+            }
+        }
+
+        Ok(expr)
     }
+
     /// factor -> power ( ( "/" | "*" ) power )*
     fn factor(&mut self) -> Result<Expr> {
-        todo!()
+        let mut expr = self.power()?;
+
+        loop {
+            let operator = match self.peek() {
+                Some(Token::Operator(op)) if matches!(op, Math::Times | Math::Division) => *op,
+                _ => break,
+            };
+            let _ = self.advance();
+
+            let right = self.power()?;
+            let prev_expr = expr;
+            expr = Expr::Binary {
+                operator: BinaryOp::Arithmetic(operator),
+                left: Box::new(prev_expr),
+                right: Box::new(right),
+            }
+        }
+
+        Ok(expr)
     }
 
     /// power -> ( ( unary "**" )* unary ) // right associative
     fn power(&mut self) -> Result<Expr> {
-        todo!()
+        // Base
+        let left = self.unary()?;
+
+        match self.peek() {
+            Some(Token::Operator(op)) if matches!(op, Math::Power) => {
+                let operator = *op; // Math::Power
+                // Consume
+                self.advance();
+
+                // Right recursion
+                let right = self.power()?;
+
+                Ok(Expr::Binary {
+                    operator: BinaryOp::Arithmetic(operator),
+                    left: Box::new(left),
+                    right: Box::new(right),
+                })
+            }
+            _ => Ok(left),
+        }
     }
 
     /// unary -> "-" unary | primary
     fn unary(&mut self) -> Result<Expr> {
-        todo!()
+        if let Some(Token::Operator(Math::Minus)) = self.peek() {
+            self.advance();
+            let right = self.unary()?;
+            Ok(Expr::Unary {
+                operator: UnaryOp::Negate,
+                right: Box::new(right),
+            })
+        } else {
+            self.primary()
+        }
     }
 
     /// primary -> NUMBER | STRING | "(" expression ")"
     fn primary(&mut self) -> Result<Expr> {
-        todo!()
+        let literal = match self.advance() {
+            Some(Token::Integer(number)) => Expr::Literal(LiteralValue::Integer(number)),
+            Some(Token::String(str)) => Expr::Literal(LiteralValue::String(str)),
+            Some(Token::Identifier(name)) => Expr::Variable { name },
+            Some(Token::Paran(Side::Left)) => {
+                let expr = self.expression()?;
+                if !matches!(self.advance(), Some(Token::Paran(Side::Right))) {
+                    bail!("Expected ')' after expression");
+                }
+                Expr::Grouping {
+                    expression: Box::new(expr),
+                }
+            }
+            _ => bail!("Expected expression"),
+        };
+
+        Ok(literal)
     }
 
     /// Consume a token and return it
